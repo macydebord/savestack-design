e2bad91545c758bd8f404bdc16586fe143478241
Anthony Arcuri <arcuriaj@mail.uc.edu>
2026-03-13 21:41:26 -0400

    Added initial homepage

---
+++ src/main/java/com/arcuriaj/savestack/HomeController.java
@@ -0,0 +1,25 @@
+package com.arcuriaj.savestack;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home(){
+        return "index";
+    }
+    @RequestMapping("/allCoupons")
+    public String allCoupons(){
+        return "allCoupons";
+    }
+    @RequestMapping("/login")
+    public String login(){
+        return "login";
+    }
+    @RequestMapping("/signup")
+    public String signup(){
+        return "signup";
+    }
+}

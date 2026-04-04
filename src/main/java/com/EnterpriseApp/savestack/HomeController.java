aba76e47deac84eaa6a73c518ecee4c3736651d2
Anthony Arcuri <arcuriaj@mail.uc.edu>
2026-04-04 16:19:17 -0400

    whoops messed up file names

--- src/main/java/com/EnterpriseApp/savestack/HomeController.java
+++ src/main/java/com/EnterpriseApp/savestack/HomeController.java
@@ -1,4 +1,4 @@
-package com.arcuriaj.savestack;
+package com.EnterpriseApp.savestack;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;

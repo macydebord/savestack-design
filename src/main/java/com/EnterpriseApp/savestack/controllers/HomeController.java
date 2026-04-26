2368ca20bb664dd8522b12a8603ee70fd6c25c5d
Mille4eg <156032134+Mille4eg@users.noreply.github.com>
2026-04-26 14:54:11 -0400

    TESTING PUSH trying to add a fake log in / user

--- src/main/java/com/EnterpriseApp/savestack/controllers/HomeController.java
+++ src/main/java/com/EnterpriseApp/savestack/controllers/HomeController.java
@@ -4,6 +4,8 @@ import com.EnterpriseApp.savestack.service.CouponService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -23,6 +25,21 @@ public class HomeController {
     @GetMapping("/login")
     public String login() {
         return "login";
+    }
+
+    @PostMapping("/login")
+    public String handleLogin(
+            @RequestParam String email,
+            @RequestParam String password) {
+
+        String testEmail = "mail@gmail.com";
+        String testPassword = "pass";
+
+        if (email.equals(testEmail) && password.equals(testPassword)) {
+            return "redirect:/";
+        }
+
+        return "redirect:/login?error=true";
     }
 
     @GetMapping("/signup")
